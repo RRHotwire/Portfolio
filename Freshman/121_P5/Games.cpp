26125dbfa80969e76dbe945761cf4aa2e2119960
RRHotwire <54288537+RRHotwire@users.noreply.github.com>
2019-08-19 13:38:30 -0700

    Add files via upload

---
+++ Freshman/121_P5/Games.cpp
@@ -0,0 +1,17 @@
+//Include guard
+#pragma once
+
+#include "Game_Master.h"
+#include "Number_Game.h"
+#include "Card_Game.h"
+#include "Nim.h"
+
+//Create Game_Master object and run conversation with user.
+//Line count = 2
+int main(){
+
+	Game_Master Game;
+	Game.Conversation();
+
+	return 0;
+}
