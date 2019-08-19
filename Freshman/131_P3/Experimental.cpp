e68a85089a8191e041310240df8af6cc06caf954
RRHotwire <54288537+RRHotwire@users.noreply.github.com>
2019-08-19 14:02:23 -0700

    Update Experimental.cpp

--- Freshman/131_P3/Experimental.cpp
+++ Freshman/131_P3/Experimental.cpp
@@ -1,19 +1,3 @@
-// Experimental.cpp
-//
-// Group member names: Cory Longshore, Josh Eden, Luke Higgott
-//
-// Project 3, CPSC 131, Spring 2016, CSU Fullerton
-//
-// Template code written in 2016 by Kevin Wortman (kwortman@fullerton.edu)
-//
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to the
-// public domain worldwide. This software is distributed without any
-// warranty. You should have received a copy of the CC0 Public Domain
-// Dedication along with this software. If not, see
-// <http://creativecommons.org/publicdomain/zero/1.0/>.
-//
-
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
